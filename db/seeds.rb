3fd1641728dc8ea1d3025176221f1bc17ee283f4
sheri <sheri.h.zada@gmail.com>
2015-12-14 10:04:46 -0800

    add more markers

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 benches = Bench.create([{
-                description: "COZYBENCH4U",
-                lat: 37.7804335,
-                lng: -122.4116771
+                  description: "COZYBENCH4U",
+                  lat: 37.7804335,
+                  lng: -122.4116771
+                },
+                {
+                  description: "FIDI Towers",
+                  lat: 37.789967,
+                  lng: -122.396056
+                },
+                {
+                  description: "Mission Retreat",
+                  lat: 37.760133,
+                  lng: -122.428736
                 }])
