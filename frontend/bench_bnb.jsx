955f3107569c43959d5c06ebf4801e44c5f66d9a
sheri <sheri.h.zada@gmail.com>
2015-12-14 09:59:19 -0800

    routers root path, index route

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,11 +1,32 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var BenchStore = require('./stores/bench');
-var ApiUtil = require('./util/apiUtil');
-var Search = require('./components/search.jsx');
+var React = require('react'),
+    ReactDOM = require('react-dom'),
+    Router = require('react-router').Router,
+    Route = require('react-router').Route,
+    IndexRoute = require('react-router').IndexRoute,
+    BenchStore = require('./stores/bench'),
+    ApiUtil = require('./util/apiUtil'),
+    Search = require('./components/search.jsx');
+
+var App = React.createClass({
+  render: function () {
+    return(
+      <div>
+        <header><h1>BenchBnB</h1></header>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Router>
+    <Route path="/" component={App}>
+      <IndexRoute component={Search}></IndexRoute>
+    </Route>
+  </Router>
+);
 
 document.addEventListener("DOMContentLoaded", function () {
-  ReactDOM.render(
-  <Search />,
-  document.getElementById('root'));
+  var root = document.getElementById('root');
+  ReactDOM.render(routes, root);
 });
